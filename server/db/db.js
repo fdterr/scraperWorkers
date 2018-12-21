e1d13e19234f8da725dcc9748fa9d2d6c9fecca0
Rick Terry <fdterr@gmail.com>
2018-12-21 17:11:04 -0500

    database url

--- server/db/db.js
+++ server/db/db.js
@@ -1,16 +1,21 @@
-const Sequelize = require('sequelize')
-const pkg = require('../../package.json')
+const Sequelize = require('sequelize');
+const pkg = require('../../package.json');
 
-const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+const databaseName =
+  pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
+
+const username = process.env.user_name;
+const password = process.env.password;
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  process.env.DATABASE_URL ||
+    `postgres://${username}:${password}@localhost:5432/${databaseName}`,
   {
     logging: false
   }
-)
-module.exports = db
+);
+module.exports = db;
 
 if (process.env.NODE_ENV === 'test') {
-  after('close database connection', () => db.close())
+  after('close database connection', () => db.close());
 }
