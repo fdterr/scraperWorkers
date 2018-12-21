2a2ba1c4f7760ee361a9e48beaa53837ca083857
Rick Terry <fdterr@gmail.com>
2018-12-21 15:25:51 -0500

    slow, but eventually completes

--- server/scraper/worker.js
+++ server/scraper/worker.js
@@ -1,52 +1,51 @@
 const ProxyVerifier = require('proxy-verifier');
 const {parentPort, workerData} = require('worker_threads');
-// const util = require('util');
 
 const checkIP = ipAddress => {
-  return new Promise((resolve, reject) => {
-    let split = ipAddress.split(':');
-    let proxy = {
-      ipAddress: split[0],
-      port: split[1],
-      protocols: ['http', 'https', 'socks4', 'socks5']
-    };
+  let split = ipAddress.split(':');
+  let proxy = {
+    ipAddress: split[0],
+    port: split[1],
+    protocols: ['http', 'https', 'socks4', 'socks5']
+  };
 
-    ProxyVerifier.testAll(proxy, (error, result) => {
-      if (error) {
-        // console.log('there would have been an error');
-        resolve(false);
-      } else if (result.anonymityLevel != null) {
-        // console.log('result: good', ipAddress);
-        // ONLY INSTANCE OF GOOD RESULT
-        resolve(result);
-      } else {
-        // console.log('there would have been an error');
-        resolve(false);
-      }
-    });
+  ProxyVerifier.testAll(proxy, (error, result) => {
+    if (error) {
+      parentPort.postMessage({
+        ip: false,
+        ...result,
+        ...error,
+        original: ipAddress,
+        id: workerData.id
+      });
+    } else if (result.anonymityLevel != null) {
+      // ONLY INSTANCE OF GOOD RESULT
+      parentPort.postMessage({
+        ip: ipAddress,
+        result,
+        original: ipAddress,
+        id: workerData.id
+      });
+    } else {
+      parentPort.postMessage({
+        ip: false,
+        ...result,
+        ...error,
+        original: ipAddress,
+        id: workerData.id
+      });
+    }
   });
 };
 
-let {list} = workerData;
-(async () => {
-  parentPort.postMessage(`thread ${workerData.id} started`);
-  if (list) {
-    for (let i = 0; i < list.length; i++) {
-      // console.log(`${workerData.id} checking ${list[i]}`);
-      let result = await checkIP(list[i]);
-      console.log(
-        `thread ${workerData.id} checked ${list[i]}, ${list.length -
-          i -
-          1} remaining`
-      );
-      // console.log(result);
-      if (result) {
-        parentPort.postMessage({ip: list[i], result});
-      }
-      // })();
-    }
-  } else {
-    console.log(`no IPs allocated to thread ${workerData.id}`);
-  }
-  parentPort.postMessage(`thread ${workerData.id} done`);
-})();
+const listen = () => {
+  parentPort.on('message', msg => {
+    checkIP(msg);
+  });
+};
+
+listen();
+let {ipAddress} = workerData;
+if (ipAddress) {
+  checkIP(ipAddress);
+}
