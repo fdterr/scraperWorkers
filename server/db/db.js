f0ce505f3b02058b51644ca0b6ef18b9c92e062a
Rick Terry <fdterr@gmail.com>
2019-01-16 17:22:38 -0500

    can run on server and with nodemon with --experimental-worker

--- server/db/db.js
+++ server/db/db.js
@@ -4,22 +4,22 @@ const pkg = require('../../package.json');
 const databaseName =
   pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
 
-const username = process.env.user_name;
+const user_name = process.env.user_name;
 const password = process.env.password;
 
-const db = new Sequelize(
-  process.env.DATABASE_URL ||
-    `postgres://${username}:${password}@localhost:5432/${databaseName}`,
-  {
-    logging: false
-  }
-);
 // const db = new Sequelize(
-//   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+//   process.env.DATABASE_URL ||
+//     `postgres://${user_name}:${password}@localhost:5432/${databaseName}`,
 //   {
 //     logging: false
 //   }
 // );
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  {
+    logging: false
+  }
+);
 
 module.exports = db;
 
