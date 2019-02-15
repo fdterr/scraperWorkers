c512202e36a4ca8afb7a6eebe2a3c22a1ae456f7
Rick Terry <fdterr@gmail.com>
2019-02-15 18:47:17 -0500

    flag images now base64

--- client/components/CheckTable.js
+++ client/components/CheckTable.js
@@ -323,7 +323,7 @@ const CheckTable = props => {
                     <img
                       src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAgFAAE8CAgAOw=="
                       className={`flag flag-${row.value.toLowerCase()}`}
-                      alt="Czech Republic"
+                      alt="Flag"
                     />
                   </div>
                   {countries[row.value]}
